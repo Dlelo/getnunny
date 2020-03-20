73a663eae737700114fd4054a06a2fb1756b9c09
dlelo <akinyidonattah@gmail.com>
2020-03-20 08:07:57 +0300

    create nunny app

--- getnunny/getnunny/urls.py
+++ getnunny/getnunny/urls.py
@@ -15,7 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse('Home Page')
+
+def about(request):
+    return HttpResponse('About Us Page')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
+    path('about/', about)
 ]
