3482bb0482b6e7aca7f41a7e2f77d32297ab5aa9
dlelo <akinyidonattah@gmail.com>
2020-06-26 06:04:21 +0300

    house owner and order model

--- getnunny/accounts/models.py
+++ getnunny/accounts/models.py
@@ -14,7 +14,34 @@ class Nunny(models.Model):
     denomination = models.CharField(max_length=100, null=True)
     contact_person = models.CharField(max_length=100, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    pay_range = models.FloatField(null=True)
+    category = models.CharField(max_length=100, null=True)
 
-    def _str_(self):
+    def __str__(self):
         return self.name
     
+class HouseOwner(models.Model):
+    CATEGORY=(
+        ('LiveIn', 'LiveIn'),
+        ('Dayburg','Dayburg'),
+        ('Emmergency','Emergency' )
+
+    )
+    name = models.CharField(max_length=100, null=True)
+    phone = models.CharField(max_length=100, null=True)
+    email = models.CharField(max_length=100, null=True)
+    religion = models.CharField(max_length=100, null=True)
+    denomination = models.CharField(max_length=100, null=True)
+    location = models.CharField(max_length=100, null=True)
+    contact_person = models.CharField(max_length=100, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+class Order(models.Model):
+    STATUS =(
+        ('Pending', 'Pending'),
+        ('Queued for allocation','Queued for allocation'),
+        ('Allocated','Allocated' )
+
+    )
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=100, null=True)
