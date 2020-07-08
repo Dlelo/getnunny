e016a00109b91fc61657b41fee479652379e98fc
dlelo <akinyidonattah@gmail.com>
2020-07-08 06:31:49 +0300

    core(feat): models for homeowners, nunny's and orders

--- getnunny/accounts/models.py
+++ getnunny/accounts/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 # Create your models here.
 class Nunny(models.Model):
+    CATEGORY=(
+        ('LiveIn', 'LiveIn'),
+        ('Dayburg','Dayburg'),
+        ('Emmergency','Emergency' )
+
+    )
     name = models.CharField(max_length=100, null=True)
     phone = models.CharField(max_length=100, null=True)
     email = models.CharField(max_length=100, null=True)
@@ -15,25 +21,19 @@ class Nunny(models.Model):
     contact_person = models.CharField(max_length=100, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     pay_range = models.FloatField(null=True)
-    category = models.CharField(max_length=100, null=True)
+    category = models.CharField(max_length=100, null=True, choices=CATEGORY)
 
     def __str__(self):
         return self.name
     
 class HouseOwner(models.Model):
-    CATEGORY=(
-        ('LiveIn', 'LiveIn'),
-        ('Dayburg','Dayburg'),
-        ('Emmergency','Emergency' )
-
-    )
     name = models.CharField(max_length=100, null=True)
     phone = models.CharField(max_length=100, null=True)
     email = models.CharField(max_length=100, null=True)
     religion = models.CharField(max_length=100, null=True)
     denomination = models.CharField(max_length=100, null=True)
     location = models.CharField(max_length=100, null=True)
-    contact_person = models.CharField(max_length=100, null=True)
+    number_of_children = models.IntegerField(null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
 
 class Order(models.Model):
@@ -43,5 +43,7 @@ class Order(models.Model):
         ('Allocated','Allocated' )
 
     )
+    houseowner = models.ForeignKey(HouseOwner, null=True, on_delete=models.SET_NULL)
+    nunny = models.ForeignKey(Nunny, null=True, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
-    status = models.CharField(max_length=100, null=True)
+    status = models.CharField(max_length=100, null=True, choices=STATUS )
