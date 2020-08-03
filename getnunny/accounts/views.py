989b18731cc7b918b92d837333fcaff78ad7d6af
dlelo <akinyidonattah@gmail.com>
2020-08-03 06:17:32 +0300

    core(feat): models for homeowners, nunny's and order

--- getnunny/accounts/views.py
+++ getnunny/accounts/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+from .models import *
 
 def home(request):
     return render(request, 'accounts/dashboard.html')
 
 def nunnys(request):
-    return render(request, 'accounts/nunnys.html')
+    nunnys = Nunny.objects.all()
+    return render(request, 'accounts/nunnys.html', {'nunnys':nunnys})
 
 def homeowner(request):
     return render(request, 'accounts/homeowner.html')
